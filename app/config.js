a4a528c9e24e1cde13142b5c072a342eca613e51
Charlie Stewart <charles.stewart@outlook.com>
2018-08-29 16:20:35 +0100

    Update for assessment
    Bringing the prototypes together into one for the assessment.

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Create an account',
+  serviceName: 'Manage my workforce data',
 
   // Default port that prototype runs on
   port: '3000',
